807e488ad172292c5e4f3cdef1f1ac8db5f60194
Monica Scroggs <scrmr0210@gmail.com>
2020-03-09 18:16:59 -0400

    added new images, clicky game score working

---
+++ clicky-game/src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center">
+    <h1>Test Your Memory: Mexican Loteria Cards</h1>
+    <p className="m-0">
+    Click on the cards to earn points, but don't click on the same card twice!
+    </p>
+  </div>
+);
+
+export default Banner;
---
+++ clicky-game/src/images.js
@@ -0,0 +1,29 @@
+import card1 from "./images/corazon.jpg";
+import card2 from "./images/el_apache.jpg";
+import card3 from "./images/el_arpa.jpg";
+import card4 from "./images/el_diablito.jpg";
+import card5 from "./images/el_melon.jpg";
+import card6 from "./images/el_nopal.jpg";
+import card7 from "./images/el_valiente.jpg";
+import card8 from "./images/el-musico-small.jpg";
+import card9 from "./images/la_sirena.jpg";
+import card10 from "./images/la_luna.jpg";
+import card11 from "./images/la_mano.jpg";
+import card12 from "./images/la_muerte.jpg";
+
+const images = [
+card1,
+card2,
+card3,
+card4,
+card5,
+card6,
+card7,
+card8,
+card9,
+card10,
+card11,
+card12,
+];
+
+export default images;
